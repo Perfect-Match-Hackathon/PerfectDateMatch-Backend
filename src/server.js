0f6293f0567ad2753dbced5bbd51cffa068f8c64
Ashley Thew <ashl.e.thew@gmail.com>
2021-04-23 21:33:50 +1200

    Remove logger

--- src/server.js
+++ src/server.js
@@ -1,7 +1,4 @@
 import './bootstrap';
 import app from './app';
-import logger from './logger';
 
-app.listen(process.env.APP_PORT, () => {
-  logger.info(`Express Application listening on port ${process.env.APP_PORT}`);
-});
+app.listen(process.env.APP_PORT);
