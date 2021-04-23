d220feba49fa514bf493d671a1f0b5b7dbdcac7d
Ashley Thew <ashl.e.thew@gmail.com>
2021-04-23 21:34:54 +1200

    Remove last logger

--- src/controllers/healthcheck.js
+++ src/controllers/healthcheck.js
@@ -1,11 +1,10 @@
 /* eslint-disable import/prefer-default-export */
-import logger from '../logger';
 
 const healthcheck = (req, res) => {
   try {
     res.sendStatus(200);
   } catch (error) {
-    logger.error(`Exception Raised in Healthcheck :: ${error.toString()}`);
+    // Do nothing
   }
 };
 
