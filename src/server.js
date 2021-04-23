68491022780a9a3606f77f326ff0246935dd60c9
Ashley Thew <ashl.e.thew@gmail.com>
2021-04-23 22:42:47 +1200

    Help me

--- src/server.js
+++ src/server.js
@@ -1,4 +1,7 @@
 import './bootstrap';
 import app from './app';
 
-app.listen(process.env.PORT || 8000);
+app.listen(process.env.PORT, () => {
+  // eslint-disable-next-line no-console
+  console.log(`Express Application listening on port ${process.env.APP_PORT}`);
+});
