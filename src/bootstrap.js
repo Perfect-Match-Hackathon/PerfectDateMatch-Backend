57274344c09480f9b9b53921ff50940f3c23e82d
Ashley Thew <ashl.e.thew@gmail.com>
2021-04-23 22:20:11 +1200

    Remove last APP_PORT

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -3,5 +3,5 @@ import dotenv from 'dotenv';
 (async () => {
   await dotenv.config();
   process.env.APPLICATION_ENV = process.env.APPLICATION_ENV || 'prod';
-  process.env.APP_PORT = process.env.APP_PORT || 3000;
+  process.env.PORT = process.env.PORT || 3000;
 })();
