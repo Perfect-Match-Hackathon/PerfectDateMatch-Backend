b8bc7cea12e61a875166f927140d739b25e9d4df
Ashley Thew <ashl.e.thew@gmail.com>
2021-04-23 22:47:31 +1200

    Print port

--- src/server.js
+++ src/server.js
@@ -1,7 +1,12 @@
+/* eslint-disable import/first */
+
+// eslint-disable-next-line no-console
+console.log(`Express Application listening on port ${process.env.PORT}`);
+
 import './bootstrap';
 import app from './app';
 
 app.listen(process.env.PORT, () => {
   // eslint-disable-next-line no-console
-  console.log(`Express Application listening on port ${process.env.APP_PORT}`);
+  console.log(`Express Application listening on port ${process.env.PORT}`);
 });
