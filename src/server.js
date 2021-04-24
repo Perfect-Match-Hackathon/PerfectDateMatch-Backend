dd46c6030e532e98e14ebc8794271e6f07c44785
Ashley Thew <ashl.e.thew@gmail.com>
2021-04-24 12:25:55 +1200

    Add firebase auth requirement

--- src/server.js
+++ src/server.js
@@ -1,10 +1,15 @@
 /* eslint-disable import/first */
 
-// eslint-disable-next-line no-console
-console.log(`Express Application listening on port ${process.env.PORT}`);
-
 import './bootstrap';
 import app from './app';
+
+const admin = require('firebase-admin');
+
+const serviceAccount = JSON.parse(process.env.FIREBASE_ADMIN_KEY);
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
 
 app.listen(process.env.PORT, () => {
   // eslint-disable-next-line no-console
