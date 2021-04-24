b390db22ee4e6a7aab2b3539c067e511418d9761
Ashley Thew <ashl.e.thew@gmail.com>
2021-04-24 13:45:12 +1200

    Finish firebase setup

--- src/server.js
+++ src/server.js
@@ -9,6 +9,7 @@ const serviceAccount = JSON.parse(process.env.FIREBASE_ADMIN_KEY);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
+  databaseURL: process.env.FIREBASE_DATABASE,
 });
 
 app.listen(process.env.PORT, () => {
