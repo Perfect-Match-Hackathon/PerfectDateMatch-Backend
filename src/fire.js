c522ea9ebbccf2638fbd8216c810ae87680a5980
Ashley Thew <ashl.e.thew@gmail.com>
2021-04-25 16:20:59 +1200

    Add basic date populator

--- src/fire.js
+++ src/fire.js
@@ -1,3 +1,5 @@
+import datePopulator from './populator/datepopulator';
+
 const admin = require('firebase-admin');
 
 const serviceAccount = JSON.parse(process.env.FIREBASE_ADMIN_KEY);
@@ -6,3 +8,12 @@ admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: process.env.FIREBASE_DATABASE,
 });
+
+admin
+  .database()
+  .ref()
+  .on('value', snapshot => {
+    if (!snapshot.hasChild('dates')) {
+      datePopulator();
+    }
+  });
