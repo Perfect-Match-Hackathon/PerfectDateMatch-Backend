9473d128de371a66a1919de2d4e74c02f0631f2b
Ashley Thew <ashl.e.thew@gmail.com>
2021-04-25 17:37:50 +1200

    Add swagger endpoints

--- src/swagger.js
+++ src/swagger.js
@@ -2,7 +2,6 @@ import swaggerUi from 'swagger-ui-express';
 import swaggerDocument from './swagger.json';
 
 const swaggerOptions = {
-  customCss: '.swagger-ui .topbar { display: none }',
   customSiteTitle: 'Perfect Match Date API',
   customfavIcon: '/assets/favicon.ico',
 };
